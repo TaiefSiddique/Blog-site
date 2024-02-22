47d31dd60affa342c080e1c17d11005a077b03cf
TaiefSiddique <taiefsiddik2@gmail.com>
2024-02-22 14:52:31 +0600

    Input tried

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,33 @@
 import './App.css'
 function App() {
 
+  const handlePost = (e) => {
+    e.preventDefault();
+    const text = e.target.value;
+    console.log(text)
+  }
+
   return (
+
     <>
-      <h1 className=''>ThoughtCanvas</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body" onSubmit={handlePost}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">What's on your mind</span>
+                </label>
+                <input type="text" placeholder="" className="input input-bordered" required />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary" type="submit">Post</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
